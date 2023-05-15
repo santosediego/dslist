905cc17231f5f71025c8f366d9a62e2d1bd89541
Diego Santos <diegosantos.dev@gmail.com>
2023-05-15 18:38:55 -0300

    Move games on list

--- src/main/java/com/santosediego/dslist/controllers/GameListController.java
+++ src/main/java/com/santosediego/dslist/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.santosediego.dslist.dto.GameListDTO;
 import com.santosediego.dslist.dto.GameMinDTO;
+import com.santosediego.dslist.dto.ReplacementDTO;
 import com.santosediego.dslist.services.GameListService;
 import com.santosediego.dslist.services.GameService;
 
@@ -19,7 +22,7 @@ public class GameListController {
 
 	@Autowired
 	private GameListService gameListService;
-	
+
 	@Autowired
 	private GameService gameService;
 
@@ -33,5 +36,10 @@ public class GameListController {
 	public List<GameMinDTO> findByList(@PathVariable Long listId) {
 		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
+	}
+
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
 	}
 }
--- src/main/java/com/santosediego/dslist/repositories/GameListRepository.java
+++ src/main/java/com/santosediego/dslist/repositories/GameListRepository.java
@@ -1,9 +1,14 @@
 package com.santosediego.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.santosediego.dslist.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 
+	@Modifying /* Quando não for um select deve adicionar o @Modifying */
+	@Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
